f0ed8a0c20419e8ee59c51f63679f5605fc09bcd
Daniel Gorin <danielgo@meta.com>
2025-09-16 07:30:26 -0700

    Create reverse_request_start_debugging.erl with startDebugging request
    Summary:
    # Context
    As part of multi-node debugging support, we need to issue a startDebugging reverse-request for each new node that attempts to reverse attach. We currently have an event system where `edb_dap_internal_events` receives a `reverse_attach` event when any node, child or parent attempts to reverse attach.
    
    # Problem
    When we receive this event in DAP, we need to issue a `startDebugging` request and need a module that creates this for us, similar to the `runInTerminal` reverse request.
    
    # This diff
    Adds `reverse_request_start_debugging.erl`, that issues a `startDebugging` request if passed with `reverse_request_start_debugging:arguments()`.
    
    Reviewed By: michalmuskala
    
    Differential Revision: D80798353
    
    fbshipit-source-id: 5a234011bc9a1bca9edb1268abb5c9cd8ff4b32a
    
    Co-authored-by: Agastya Bahl <agastyabahl@meta.com>

---
+++ edb/src/edb_dap_reverse_request_start_debugging.erl
@@ -0,0 +1,56 @@
+%% Copyright (c) Meta Platforms, Inc. and affiliates.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
+%%% % @format
+
+-module(edb_dap_reverse_request_start_debugging).
+
+%% erlfmt:ignore
+% @fb-only
+-compile(warn_missing_spec_all).
+
+-behaviour(edb_dap_reverse_request).
+
+-export([make_request/1, handle_response/2]).
+
+%% ------------------------------------------------------------------
+%% Types
+%% ------------------------------------------------------------------
+
+% https://microsoft.github.io/debug-adapter-protocol//specification.html#Reverse_Requests_StartDebugging
+-type arguments() ::
+    #{
+        request := attach,
+        configuration := edb_dap_request_attach:arguments()
+    }
+    | #{
+        request := launch,
+        configuration := edb_dap_request_launch:arguments()
+    }.
+
+-type response_body() :: #{}.
+
+-export_type([arguments/0, response_body/0]).
+
+%% ------------------------------------------------------------------
+%% Behaviour implementation
+%% ------------------------------------------------------------------
+-spec make_request(Args) -> edb_dap_reverse_request:request(Args) when
+    Args :: arguments().
+make_request(Args) ->
+    #{command => ~"startDebugging", arguments => Args}.
+
+-spec handle_response(edb_dap_server:state(), response_body()) -> edb_dap_reverse_request:reaction().
+handle_response(_State0, _Body) ->
+    #{}.
