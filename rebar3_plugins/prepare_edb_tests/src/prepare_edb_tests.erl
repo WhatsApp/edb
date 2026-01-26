68ec9d3d2da6076713176445ac5ebb929894f81b
Daniel Gorin <danielgo@meta.com>
2026-01-26 12:03:43 -0800

    Add rebar3 plugin to handle edb test dependencies (#17)
    Summary:
    
    # Context
    The tests of the `edb` application assume that the `edb` binary is available in the resource directory of the suite.
    
    # Problem
    We've been using a hacky pre-hook to ensure this, but it doesn't scale well, and for windows the logic needs to be different
    
    # This diff
    We instead introduce an ad-hoc rebar3 plugin that essentially does the same: copies the edb  binary from the escript to the resource directory of every suite
    
    Reviewed By: robertoaloi, michalmuskala
    
    Differential Revision: D91346198

---
+++ rebar3_plugins/prepare_edb_tests/src/prepare_edb_tests.erl
@@ -0,0 +1,74 @@
+-module(prepare_edb_tests).
+-compile(warn_missing_spec_all).
+-oncall("was_devx").
+
+-export([init/1, do/1, format_error/1]).
+
+-include_lib("kernel/include/file.hrl").
+
+-define(PROVIDER, prepare_edb_tests).
+-define(DEPS, [escriptize]).
+
+-spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
+init(State) ->
+    Provider = providers:create([
+        {name, ?PROVIDER},
+        {module, ?MODULE},
+        {bare, true},
+        {deps, ?DEPS},
+        {example, "rebar3 prepare_edb_tests"},
+        {short_desc, "Copy edb binary to test data directories"},
+        {desc, "Copies the edb binary to all test suite data directories in edb app"},
+        {opts, []}
+    ]),
+    {ok, rebar_state:add_provider(State, Provider)}.
+
+-spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, term()}.
+do(State) ->
+    rebar_api:info("Copying edb binary to test data directories...", []),
+
+    App = rebar_state:current_app(State),
+    ProfileDir = rebar_dir:profile_dir(rebar_state:opts(State), rebar_state:current_profiles(State)),
+    BinarySrc = filename:join([ProfileDir, "bin", "edb"]),
+
+    case filelib:is_file(BinarySrc) of
+        false ->
+            {error, {?MODULE, {binary_not_found, BinarySrc}}};
+        true ->
+            SrcTestDir = filename:join([rebar_app_info:dir(App), "test"]),
+            Suites = discover_suites(SrcTestDir),
+
+            OutTestDir = filename:join([rebar_app_info:out_dir(App), "test"]),
+
+            lists:foreach(
+                fun(Suite) ->
+                    DataDir = filename:join([OutTestDir, Suite ++ "_data"]),
+                    ok = filelib:ensure_path(DataDir),
+                    Dest = filename:join([DataDir, "edb"]),
+                    {ok, _} = file:copy(BinarySrc, Dest),
+                    case os:type() of
+                        {unix, _} ->
+                            {ok, #file_info{mode = Mode}} = file:read_file_info(BinarySrc),
+                            file:change_mode(Dest, Mode);
+                        _ ->
+                            ok
+                    end
+                end,
+                Suites
+            ),
+            rebar_api:info("Copied binary to ~p test suites", [length(Suites)]),
+            {ok, State}
+    end.
+
+-spec format_error(term()) -> iolist().
+format_error({binary_not_found, Path}) ->
+    io_lib:format("edb binary not found at ~s", [Path]);
+format_error(Reason) ->
+    io_lib:format("~p", [Reason]).
+
+%% Discover all test suites in the edb app
+-spec discover_suites(file:filename()) -> [string()].
+discover_suites(TestDir) ->
+    Pattern = filename:join([TestDir, "*_SUITE.erl"]),
+    Files = filelib:wildcard(Pattern),
+    [filename:basename(F, ".erl") || F <- Files].
